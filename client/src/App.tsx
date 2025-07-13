f1d07c63b4310d8c7a61b587e1c2d6752974cdf9
Jose Manuel Mamani Villalpando <villalpando682@gmail.com>
2025-07-13 12:25:53 -0400

    Mejoras aplicadas en componentes y estilo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ import ListaProductos from './ListaProductos';
     <div className="App">
       <header>
       <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-        <img src={logo} alt="Logo" style={{ width: '100px', height: '70px' }} />
+        <img src={logo} alt="Logo" style={{ width: '100px', height: '50px' }} />
         <h1> LA TIENDA TIENDA DE HALLY</h1>
       </div>
       </header>
